8629e00058c9cdd77f94e386602c3c71a3e6d35d
Christing <sfterminator@126.com>
2019-01-25 10:09:43 +0800

    2019-01-25 -> 解析韩剧TV数据

---
+++ app/src/main/java/com/aizen/wanandroid/korea/Place.java
@@ -0,0 +1,11 @@
+package com.aizen.wanandroid.korea;
+
+/**
+ * Created by ld on 2019/1/25.
+ *
+ * @author ld
+ * @date 2019/1/25
+ * 描    述：
+ */
+public class Place {
+}
---
+++ app/src/main/java/com/aizen/wanandroid/korea/controller/KDramaMainActivity.java
@@ -0,0 +1,13 @@
+package com.aizen.wanandroid.korea.controller;
+
+/**
+ * Created by ld on 2019/1/25.
+ *
+ * @author ld
+ * @date 2019/1/25
+ * 描    述：韩剧TV主页控制器
+ */
+public class KDramaMainActivity {
+
+
+}
